eea7cedad5b0822ebee709d4764fa94f5cb0461c
biscotty666 <biscotty666@gmail.com>
2023-01-21 17:50:04 -0700

    That's it for a while

--- D3Force/src/App.js
+++ D3Force/src/App.js
@@ -1,51 +1,68 @@
 // import { useEffect, useState, } from "react";
 import "./App.css";
-import { forceSimulation, forceManyBody, forceLink, forceCenter, select } from 'd3'
+import {
+  forceSimulation,
+  forceManyBody,
+  forceLink,
+  forceCenter,
+  select,
+  drag
+} from 'd3'
+import { nodes, links, MANY_BODY_STRENGTH } from "./data";
 
 const App = () => {
-  const nodes = [
-    { "id": "Alice" },
-    { "id": "Bob" },
-    { "id": "Carol" }
-  ];
-
-  const links = [
-    { source: 0, target: 1 },
-    { source: 1, target: 2 }
-  ];
-
   const svg = select('#container')
-
   const width = +svg.attr('width')
   const height = +svg.attr('height')
-
   const centerX = width / 2
   const centerY = height / 2
 
   const simulation = forceSimulation(nodes)
-    .force("charge", forceManyBody())
-    .force("link", forceLink(links))
+    .force("charge", forceManyBody().strength(MANY_BODY_STRENGTH))
+    .force("link", forceLink(links).distance((link) => link.distance))
     .force("center", forceCenter(centerX, centerY));
 
-  const circles = svg
-    .selectAll('circle')
-    .data(nodes)
-    .enter()
-    .append('circle')
-    .attr('r', 10)
+  const dragInteraction = drag().on("drag", (event, node) => {
+    node.fx = event.x
+    node.fy = event.y
+    simulation.alpha(1)
+    simulation.restart()
+  })
 
   const lines = svg
     .selectAll('line')
     .data(links)
     .enter()
     .append('line')
-    .attr('stroke', 'black')
+    .attr('stroke', link => link.color || 'black')
+
+  const circles = svg
+    .selectAll('circle')
+    .data(nodes)
+    .enter()
+    .append('circle')
+    .attr('fill', (node) => node.color || 'gray')
+    .attr('r', node => node.size)
+    .call(dragInteraction)
+
+  const text = svg
+    .selectAll('text')
+    .data(nodes)
+    .enter()
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
+    .style('pointer-events', 'none')
+    .text(node => node.id)
 
 
   simulation.on('tick', () => {
     circles
       .attr('cx', (node) => node.x)
       .attr('cy', (node) => node.y)
+    text
+      .attr('x', (node) => node.x)
+      .attr('y', (node) => node.y)
     lines
       .attr('x1', link => link.source.x)
       .attr('y1', link => link.source.y)
--- D3Force/src/data.js
+++ D3Force/src/data.js
@@ -1,23 +1,23 @@
-import { colors } from './colors';
+import { colors } from "./colors";
 
-export const nodes = [];
+export const nodes = []
+
 export const links = [];
 
-const MAIN_NODE_SIZE = 40;
-const CHILD_NODE_SIZE = 15;
-const LEAF_NODE_SIZE = 5;
-const DEFAULT_DISTANCE = 20;
-const MAIN_NODE_DISTANCE = 90;
-const LEAF_NODE_DISTANCE = 30;
-export const MANY_BODY_STRENGTH = -20;
+const MAIN_NODE_SIZE = 40
+const CHILD_NODE_SIZE = 15
+const LEAF_NODE_SIZE = 5
+const DEFAULT_DISTANCE = 30
+const LEAF_NODE_DISTANCE = 20
+const MAIN_NODE_DISTANCE = 90
+export const MANY_BODY_STRENGTH = -10
 
-let i = 0;
-
+let i=0
 const addMainNode = (node) => {
-  node.size = MAIN_NODE_SIZE;
-  node.color = colors[i++][1];
-  nodes.push(node);
-};
+  node.size = MAIN_NODE_SIZE
+  node.color = colors[i++][1]
+  nodes.push(node)
+}
 
 const addChildNode = (
   parentNode,
@@ -25,25 +25,25 @@ const addChildNode = (
   size = CHILD_NODE_SIZE,
   distance = DEFAULT_DISTANCE
 ) => {
-  childNode.size = size;
-  childNode.color = parentNode.color;
-  nodes.push(childNode);
-  links.push({
-    source: parentNode,
-    target: childNode,
-    distance,
-    color: parentNode.color,
-  });
-};
+  childNode.size = size
+  childNode.color = parentNode.color
+  nodes.push(childNode)
+  links.push({ 
+    source: parentNode, 
+    target: childNode, 
+    distance: distance,
+    color: parentNode.color
+  })
+}
 
 const assembleChildNode = (parentNode, id, numLeaves = 20) => {
-  const childNode = { id };
-  addChildNode(parentNode, childNode);
+  const childNode = { id }
+  addChildNode(parentNode, childNode)
 
   for (let i = 0; i < numLeaves; i++) {
-    addChildNode(childNode, { id: '' }, LEAF_NODE_SIZE, LEAF_NODE_DISTANCE);
+    addChildNode(childNode, { id: "" }, LEAF_NODE_SIZE, LEAF_NODE_DISTANCE)
   }
-};
+}
 
 const connectMainNodes = (source, target) => {
   links.push({
@@ -51,40 +51,47 @@ const connectMainNodes = (source, target) => {
     target,
     distance: MAIN_NODE_DISTANCE,
     color: source.color
-  });
-};
+  })
+}
 
-const artsWeb = { id: 'Arts Web' };
-addMainNode(artsWeb);
-assembleChildNode(artsWeb, 'Community Vision');
-assembleChildNode(artsWeb, 'Silicon Valley Creates');
+const artsWeb = { id: "Arts Web" }
+addMainNode(artsWeb)
+assembleChildNode(artsWeb, 'Coummunity Vision')
+assembleChildNode(artsWeb, 'Silicon Valley Creates')
 
-const socialImpactCommons = { id: 'Social Impact Commons' };
-addMainNode(socialImpactCommons);
-assembleChildNode(socialImpactCommons, 'Theatre Bay Area');
-assembleChildNode(socialImpactCommons, 'EastSide Arts Alliance');
-assembleChildNode(socialImpactCommons, 'Local Color');
+const socialImpactCommons = { id: "Social Impact Commons" }
+addMainNode(socialImpactCommons)
+assembleChildNode(socialImpactCommons, 'Theater Bay Area')
+assembleChildNode(socialImpactCommons, 'EastSide Arts')
+assembleChildNode(socialImpactCommons, 'Local Color')
 
-const cast = { id: 'Community Arts Stabilization Trust' };
-addMainNode(cast);
-assembleChildNode(cast, 'CounterPulse');
-assembleChildNode(cast, 'Luggage Store Gallery');
-assembleChildNode(cast, 'Performing Arts Workshop');
-assembleChildNode(cast, '447 Minna St.', 5);
-assembleChildNode(cast, 'Keeping Space Oakland');
+const cast = { id: "Community Arts Stabilization Trust" }
+addMainNode(cast)
+assembleChildNode(cast, 'CounterPulse')
+assembleChildNode(cast, 'Luggage Store Gallery')
+assembleChildNode(cast, 'Performing Arts Workshop')
+assembleChildNode(cast, '447 Minna St.', 5)
+assembleChildNode(cast, 'Keeping Space Oakland')
 
-const ambitioUS = { id: 'AmbitioUS' };
-addMainNode(ambitioUS);
-assembleChildNode(ambitioUS, 'EBPREC');
-assembleChildNode(ambitioUS, 'SELC', 3);
-assembleChildNode(ambitioUS, 'The Runway Project', 3);
-assembleChildNode(ambitioUS, 'Common Future', 3);
-assembleChildNode(ambitioUS, 'Freelancers Union', 3);
-assembleChildNode(ambitioUS, 'US Federation of Worker Cooperatives', 3);
+const ambitioUS = { id: "AmbitioUS" }
+addMainNode(ambitioUS)
+assembleChildNode(ambitioUS, 'EBPREC')
+assembleChildNode(ambitioUS, 'SELC', 3)
+assembleChildNode(ambitioUS, 'The Runway Project', 0)
+assembleChildNode(ambitioUS, 'Common Future', 0)
+assembleChildNode(ambitioUS, 'Freelancers Union', 0)
+assembleChildNode(ambitioUS, 'US Federation of Worker Cooperatives', 0)
 
-connectMainNodes(artsWeb, socialImpactCommons);
-connectMainNodes(artsWeb, cast);
-connectMainNodes(socialImpactCommons, cast);
-connectMainNodes(ambitioUS, cast);
-connectMainNodes(ambitioUS, socialImpactCommons);
-connectMainNodes(ambitioUS, artsWeb);
+
+connectMainNodes(artsWeb, socialImpactCommons)
+connectMainNodes(artsWeb, cast)
+connectMainNodes(socialImpactCommons, cast)
+connectMainNodes(ambitioUS, artsWeb)
+connectMainNodes(ambitioUS, socialImpactCommons)
+connectMainNodes(ambitioUS, cast)
+
+
+
+
+
+
